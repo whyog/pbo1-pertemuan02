ad3bbb4957a538f4e05cba49c600103af4d84505
whyog <yogadrk@gmail.com>
2024-03-31 15:10:38 +0800

    Memanggil method  dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ===================");
+  System.out.println("NPM   : 2210010652");
+  System.out.println("Nama  : Nur Yoga Andika");
+  System.out.println("=====================================");
+}
+}
---
+++ model/MataKuliah.java
@@ -0,0 +1,11 @@
+package model;
+
+public class MataKuliah{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Matak Kuliah ===================");
+  System.out.println("Kode  : TIF3402");
+  System.out.println("Nama  : Pemrograman Berbasis Objek 1");
+  System.out.println("=======================================");
+}
+}
